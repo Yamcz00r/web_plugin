7553e87731b66222125bb47b3c5c6c652ade9969
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2023-11-01 11:09:19 +0100

    still playing around

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,60 @@
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, status, HTTPException
 from typing import Annotated
+from pydantic import BaseModel
 
 
+class User(BaseModel):
+    id: int
+    email: str
+    password: str
 
-# class Item(BaseModel):
-#     id: int
-#     name: str
-#     price: int
-#     description: str | None = None
-
-items = []
+users = []
 
 app = FastAPI()
 
-@app.get('/items/')
-def user_agent(
-    user_agent: Annotated[str | None, Header()] = None, host: Annotated[str | None, Header()] = None,
-    accept: Annotated[str | None, Header()] = None
-    ):
-    return {"User-Agent": user_agent, "host": host, "accept": accept}
+
+@app.get('/user')
+async def get_users() -> list[User]:
+    if len(users) == 0:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="There are no users")
+    return {
+       users
+    }
+
+@app.post('/user/create')
+async def login(user_data: User):
+    for user in users:
+        if user_data.id == user.id:
+            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="User is already existing")
+    users.append(user_data)
+    return {
+        "message": "Succesfully created a user",
+        "data": user_data
+    }  
+    
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+# Reciving a headers
+# @app.get('/items/')
+# def user_agent(
+#     user_agent: Annotated[str | None, Header()] = None, host: Annotated[str | None, Header()] = None,
+#     accept: Annotated[str | None, Header()] = None
+#     ):
+#     return {"User-Agent": user_agent, "host": host, "accept": accept}
 
 
 
