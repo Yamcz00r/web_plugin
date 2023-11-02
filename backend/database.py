647cb4a84f502e6f834d4af06a2a10114ec1b3e9
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2023-11-02 14:48:19 +0100

    working around

---
+++ backend/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+
+url = "postgresql://postgres:zaq12wsx@localhost:5432/testing"
+engine = create_engine(
+    url, 
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
--- backend/main.py
+++ backend/main.py
@@ -1,38 +1,49 @@
-from fastapi import FastAPI, status, HTTPException
-from typing import Annotated
-from pydantic import BaseModel
+from fastapi import FastAPI, status, HTTPException, Depends
+from sqlalchemy.orm import Session
 
+import crud, models, schemas
+from .database import SessionLocal, engine
 
-class User(BaseModel):
-    id: int
-    email: str
-    password: str
-
-users = []
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+#Dependency
 
-@app.get('/user')
-async def get_users() -> list[User]:
-    if len(users) == 0:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="There are no users")
-    return {
-       users
-    }
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally: 
+        db.close() 
 
-@app.post('/user/create')
-async def login(user_data: User):
-    for user in users:
-        if user_data.id == user.id:
-            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="User is already existing")
-    users.append(user_data)
-    return {
-        "message": "Succesfully created a user",
-        "data": user_data
-    }  
-    
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="User with this email is already existing")
+    return crud.create_user(db=db, user=user)
 
+@app.get("/users/", response_model=list[schemas.User])
+def read_user(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User cannot be found")
+    return db_user
+
+@app.post("/users/{user_id}/items", response_model=schemas.Item)
+def create_item(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
 
     
 
