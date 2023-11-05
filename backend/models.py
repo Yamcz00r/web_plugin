e9f21b3485ba202e8d2528fed3761208b8c21674
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2023-11-05 11:20:33 +0100

    qwqw

--- backend/models.py
+++ backend/models.py
@@ -7,3 +7,17 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    items = relationship("items", back_populates="owner")
+
+class Items(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("users", back_populates="items")
+
+
