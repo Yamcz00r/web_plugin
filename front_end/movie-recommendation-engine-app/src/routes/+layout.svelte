5e7a79e7d60d872cc4eef63817f531385cbbaf72
Nekxis <nexispolska16@gmail.com>
2023-11-20 18:28:11 +0100

    sign in

---
+++ front_end/movie-recommendation-engine-app/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import '../app.postcss';
+    import {AppShell, Drawer} from "@skeletonlabs/skeleton";
+    import Header from "./Header.svelte";
+    import { initializeStores } from '@skeletonlabs/skeleton';
+
+    initializeStores();
+</script>
+<Drawer>
+
+</Drawer>
+
+<AppShell>
+    <svelte:fragment slot="pageHeader">
+        <Header/>
+    </svelte:fragment>
+    <slot/>
+</AppShell>
---
+++ front_end/movie-recommendation-engine-app/src/routes/auth/signin/+page.svelte
@@ -0,0 +1,23 @@
+
+<div class="w-full flex justify-center bg-opacity-0">
+
+    <form class="w-1/4 flex flex-col p-4">
+        <h2 class="h2 self-center"> Sign In </h2>
+        <label class="label py-3">
+            <span>Email</span>
+            <input class="input" title="Input (email)" type="email" placeholder="john@example.com"
+                   autocomplete="email"/>
+        </label>
+        <label class="label py-3">
+            <span class="justify-self-center">Password</span>
+            <input class="input" title="Input (password)" type="password" placeholder="password"/>
+        </label>
+        <label class="label py-3">
+            <span>Password</span>
+            <input class="input" title="Input (password)" type="password" placeholder="password"/>
+        </label>
+        <button type="submit" class="btn variant-filled mt-8">Sing In</button>
+    </form>
+</div>
+
+
