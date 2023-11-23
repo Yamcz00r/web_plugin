ddd4c1de94433297ff550aba0202735d74eea227
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2023-11-23 15:17:35 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 connection_url = env.get("POSTGRES_CONNECTION_URL")
-
+print(connection_url)
 engine = create_engine(connection_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
