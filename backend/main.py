ce1ba8ea94ef694dca231b03f9edc81dcefefdaf
pawelcp <94795184+pawelcp@users.noreply.github.com>
2023-11-24 12:26:49 +0100

    docker

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def hello_world():
-    return {"message": "Hello, World"}
-
-@app.get("/users")
-def get_users():
-    return {"users": []}
+def read_root():
+    return {"123": "11"}
