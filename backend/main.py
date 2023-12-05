5c14ba36011df4e4eeb32a22a7c0bce20f19f24b
pawelcp <94795184+pawelcp@users.noreply.github.com>
2023-12-05 17:33:35 +0100

    delete_user

--- backend/main.py
+++ backend/main.py
@@ -1,32 +1,30 @@
-# main.py
-
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from utils.database_utils import get_db
-from services.user_service import create_user, get_user_by_emial
+from services.user_service import create_user, get_user_by_emial, delete_user
 import schemas.user_schema as user_schema
 import os
 from utils.database_utils import Base, engine
 
 app = FastAPI()
 
-BaseEngine =  Base.metadata.create_all(bind=engine)
-
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
     return {"Hello": os.environ.get("DB_CONNECTION_URL", "No DB Connection URL")}
 
-@app.post("/create_user/")
-def create_user(user_data: user_schema.UserCreate, db: Session = Depends(get_db)):
+@app.post("/users/create_user/")
+def create_user_endpoint(user_data: user_schema.UserCreate, db: Session = Depends(get_db)):
     existing_user = get_user_by_emial(db, user_data.email)
     if existing_user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Użytkownik o tej nazwie już istnieje",
+            detail="A user with this email address already exists",
         )
 
     return create_user(db, user_data)
 
-# @app.post("/delete_user/${update_user_id}")
-# async def delete_user(db: Session, )
+@app.post("/users/delete_user/{delete_user_id}")
+def delete_user_endpoint(delete_user_id: int, db: Session = Depends(get_db)):
+    return delete_user(db=db, user_id=delete_user_id)
