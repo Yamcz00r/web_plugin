f46c42cbe7cbf66ed7c4c3270ca85da2c445330f
Nekxis <nexispolska16@gmail.com>
2023-12-18 15:03:17 +0100

    id + comment

--- front_end/secure-comment/src/routes/utils/dataScraper.svelte
+++ front_end/secure-comment/src/routes/utils/dataScraper.svelte
@@ -2,21 +2,21 @@
     import {extractHtml} from "./dataExtractor";
     import {data} from "../store"
 
-    const regexUser = "z";
+    const regexUser = /<a id="author-text" class="yt-simple-endpoint style-scope ytd-comment-view-model"[^>]*>[\s\S]*?<\/a>/g;
     const regexComment = /<span class="yt-core-attributed-string yt-core-attributed-string--white-space-pre-wrap" dir="auto"[^>]*>[\s\S]*?<\/span>/g;
+
     export async function handleExtractHtml() {
         const comments = await extractHtml();
         if (comments) {
-            data.set(comments)
             let separatedComments = comments.split('<ytd-comment-thread-renderer class="style-scope ytd-item-section-renderer">');
-            let commentsList = {
-                test: "test",
-            };
+            let commentsList = {};
             separatedComments.forEach((comment) => {
-                commentsList[Object.keys(commentsList).length] = comment.match(regexComment);
+                const user = comment.match(regexUser);
+                const commentElement = comment.match(regexComment);
+                commentsList[Object.keys(commentsList).length] = [user, commentElement];
             })
+            data.set(JSON.stringify(commentsList));
             console.log(separatedComments, commentsList)
         }
     }
-
 </script>
