eff02199ccac43f1d1800495098f25fb11f217de
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2024-01-05 23:22:30 +0100

    change cors originis

--- backend/main.py
+++ backend/main.py
@@ -12,11 +12,10 @@ from schemas.comment_schema import Comments
 
 app = FastAPI()
 
-origins = ["http://127.0.0.1:8000", "http://localhost:8000","http://127.0.0.1:3000", "http://localhost:3000", "http://127.0.0.1:5173", "http://localhost:5173"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
